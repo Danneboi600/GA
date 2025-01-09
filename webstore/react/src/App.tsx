0a0e16a5dc341d08456eadf7f6a9eb1ccbce4117
Danneboi600 <148189262+Danneboi600@users.noreply.github.com>
2025-01-09 08:40:34 +0000

    df

--- webstore/react/src/App.tsx
+++ webstore/react/src/App.tsx
@@ -1,10 +1,16 @@
 import ListGroup from "./components/ListGroup";
+//import Footer from "./components/Footer";
 
 function App() {
   let items = ["T-Shirts", "Jeans", "Hats", "Sweatshirts", "Accessories"];
+
+    const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
-      <ListGroup items={items} heading="Clothes"/>
+      <ListGroup items={items} heading="Clothes" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
---
+++ webstore/react/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+/*
+interface FooterProps {
+    items: String[];
+}
+
+function Footer({items}: FooterProps ) {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Footer;*/
